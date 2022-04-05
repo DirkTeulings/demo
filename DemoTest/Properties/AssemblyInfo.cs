0d7ca8150ac3eda8b830d01b2d456b82abc605c0
Dirk Teulings <dirk.teulings@iodigital.com>
2022-04-05 14:44:35 +0200

    Workflow test

---
+++ DemoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DemoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49cac051-2714-42bd-82fd-5908aee8eea8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DemoTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DemoTest
+{
+    [TestClass]
+    public class DemoTests
+    {
+        /// <summary>
+        /// is true actually true
+        /// </summary>
+        [TestMethod]
+        public void IsTrueTrue()
+        {
+            Assert.IsTrue(true);
+        }
+
+        /// <summary>
+        /// is false actually false
+        /// </summary>
+        [TestMethod]
+        public void IsFalseFalse()
+        {
+            Assert.IsFalse(false);
+        }
+
+        /// <summary>
+        /// wonkey test
+        /// </summary>
+        [TestMethod]
+        public void Wonkey()
+        {
+            var val = true;
+
+            if (true == val)
+                if (!true == !val)
+                    if (!false == val)
+                    {
+                        Assert.IsTrue(val);
+                        return;
+                    }
+                        
+
+            Assert.IsFalse(val);
+
+        }
+    }
+}
