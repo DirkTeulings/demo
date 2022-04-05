dba1da041ceb98484b63596eef84075ed5381d41
Dirk Teulings <dirk.teulings@iodigital.com>
2022-04-05 10:32:28 +0200

    More files
    This is just way better

---
+++ Demo/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Model
+{
+    public class User : Displayable
+    {
+        private string _name;
+        public string name { set { _name = value; } }
+
+        private string _age;
+        public string age { set { _age = value; } }
+
+        private Address _address;
+        public Address address { set { _address = value; } }
+
+        public override string GetDisplayText()
+        {
+            return $"{_name} you are {_age} years old and live at {_address.GetDisplayText()}";
+        }
+    }
+}
