b46196f2148ad0f333f917e987d848e7ba1cfc8e
Dirk Teulings <dirk.teulings@iodigital.com>
2022-04-05 10:00:02 +0200

    multiplication

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -10,6 +10,13 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Multiplication first number:");
+            var first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Multiplication second number:");
+            var second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Result: {first * second}");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
         }
     }
 }
